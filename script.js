25765876d57fe888694c3f53fc9a44cb5c20e178
Matheus <matheuslopes.instrutor@gmail.com>
2025-12-20 06:37:14 -0300

    adicionando mapa

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ document.querySelectorAll('.link-btn').forEach(btn => {
     btn.style.transform = "scale(0.95)";
     setTimeout(() => btn.style.transform = "scale(1)", 150);
   });
+
+  function abrirMapa() {
+  const endereco = encodeURIComponent(
+    "R. Castro Alves, 331 - Centro, Gov. Valadares - MG, 35010-310"
+  );
+
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  const isIOS = /iPad|iPhone|iPod/.test(userAgent);
+
+  if (isIOS) {
+    // Apple Maps
+    window.location.href = `https://maps.apple.com/?q=${endereco}`;
+  } else {
+    // Google Maps (Android)
+    window.location.href = `https://www.google.com/maps/search/?api=1&query=${endereco}`;
+  }
+}
+
 });
