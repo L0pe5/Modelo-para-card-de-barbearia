da923e7d952bc0188e981d2b5e74284594934d79
Matheus Lopes <matheuslopes.instrutor@gmail.com>
2026-04-08 09:31:07 -0300

    Correções com numero do yury

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// Efeito de clique nos botões
-document.querySelectorAll('.link-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    btn.style.transform = "scale(0.95)";
-    setTimeout(() => btn.style.transform = "scale(1)", 150);
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.querySelectorAll('.link-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.style.transform = "scale(0.95)";
+      setTimeout(() => btn.style.transform = "scale(1)", 150);
+    });
   });
+
 });
 
-// ================= MAPA MOBILE =================
+// MAPA
 function abrirMapa() {
   const endereco = encodeURIComponent(
-    "R. Castro Alves, 331 - Centro, Gov. Valadares - MG, 35010-310"
+    "R. Castro Alves, 331 - Centro, Gov. Valadares - MG"
   );
 
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  const isIOS = /iPad|iPhone|iPod/.test(userAgent);
+  const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
 
   if (isIOS) {
-    // Apple Maps (iPhone)
     window.location.href = `https://maps.apple.com/?q=${endereco}`;
   } else {
-    // Google Maps (Android)
     window.location.href = `https://www.google.com/maps/search/?api=1&query=${endereco}`;
   }
 }
